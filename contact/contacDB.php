0e608fdc90c4be830478ca7de6a40d83410a9a9b
Nikila99gimhan <nikila99fernando.gimhan@gmail.com>
2020-12-30 15:07:38 +0530

    create database and php bage for contac us

---
+++ contact/contacDB.php
@@ -0,0 +1,35 @@
+<?php
+
+//connect to he sever
+include '../connection.php';
+
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$massage =$_POST['Tbox'];
+$country =$_POST['country'];
+
+
+//query
+
+$qry = "INSERT INTO contac VALUES  ('$fname','$lname','$country','$$massage')";
+$result =mysqli_query($con,$qry);
+
+if ($result ==true)
+{
+    echo "Data inserted Successfully!";
+}
+
+else
+{
+    die("Error inserting data " .mysqli_error($con));
+
+}
+
+$to = "seasidesouthpark@fgmail.com";
+$headers ="from :$email \r\n";
+
+
+
+
+
+?>
